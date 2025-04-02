42fe06f552801658f6d261d2cefc77b53f45122b
Gonzalo Fernandez <lfernandezbogarin@frba.utn.edu.ar>
2025-04-02 13:10:02 -0300

    Ejercicios 1 y 2 de SQL + libros

---
+++ GestionDeDatos/SQL/Ejercicio2.sql
@@ -0,0 +1,31 @@
+/* Mostrar el código, detalle de todos los artículos vendidos en el año 2012 ordenados por 
+cantidad vendida. */
+
+USE GD2025C1
+
+SELECT prod_codigo, prod_detalle, SUM(item_cantidad) AS Cantidad_Vendida
+FROM Producto
+JOIN Item_Factura ON item_producto = prod_codigo
+JOIN Factura ON fact_numero = item_numero
+WHERE YEAR(fact_fecha) = 2012
+GROUP BY prod_codigo, prod_detalle
+ORDER BY SUM(item_cantidad) DESC
+
+
+/*  CONSULTA OPTIMIZADA:
+
+WHERE YEAR(fact_fecha) = 2012 ----> WHERE fact_fecha >= '2012-01-01' AND fact_fecha < '2013-01-01'
+
+  ** El uso de YEAR(fact_fecha) = 2012 en el WHERE puede afectar la eficiencia de la consulta si fact_fecha está indexada. 
+     Esto se debe a que aplicar YEAR() sobre una columna impide que el índice se use correctamente.
+
+SELECT p.prod_codigo, p.prod_detalle, SUM(i.item_cantidad) AS Cantidad_Vendida
+FROM Producto p
+JOIN Item_Factura i ON i.item_producto = p.prod_codigo
+JOIN Factura f ON f.fact_numero = i.item_numero
+WHERE f.fact_fecha >= '2012-01-01' 
+  AND f.fact_fecha < '2013-01-01'
+GROUP BY p.prod_codigo, p.prod_detalle
+ORDER BY Cantidad_Vendida DESC;
+
+  */
